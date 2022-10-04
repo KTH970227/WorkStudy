6626717652a4737cb365a09515b1291afbcd44c1
TaeHyoung Kim <98598180+KTH970227@users.noreply.github.com>
2022-10-04 21:51:40 +0900

    C workspace start

---
+++ Xcode/C_workspace/C_workspace/C_01_Variable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//변수(Variable) : 일반적이고 확장된 개념의 기능 함수라 생각함.
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+    
+    x = 1;
+    y = 2;
+    z = x + y;
+    
+    printf("Result is %i\n", z);
+    
+    return 0;
+}
